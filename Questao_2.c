5a0152ee5102a8bf5fe292329095dcf81033c813
dev-americo <dev.americo@gmail.com>
2025-10-17 18:20:03 -0300

    logica de interação
    Co-authored-by: dev-LucasDpaula <dev-LucasDpaula@users.noreply.github.com>

--- Questao_2.c
+++ Questao_2.c
@@ -12,4 +12,22 @@ int main() {
         scanf("%d", &vetor[i]);
     }
 
+    boolean ativas = false;
+    int ano = 0;
+
+    while (ativas == false && ano < 50){
+        
+        ano++;
+        ativas = true;
+
+        for(int i = 0; i < (numero_itens + 1); i++){
+            if (ano % vetor[i] != 0){
+                ativas = false;
+            }
+        }
+
+    }
+
 }
+
+Co-authored-by: dev-LucasDpaula <dev-LucasDpaula@users.noreply.github.com>
