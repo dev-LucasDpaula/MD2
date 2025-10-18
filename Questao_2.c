408bb38a22bafcd004ad7daf0613a94888581987
dev-americo <dev.americo@gmail.com>
2025-10-17 23:28:40 -0300

    refatoração

--- Questao_2.c
+++ Questao_2.c
@@ -1,40 +1,51 @@
 #include <stdio.h>
 
+// Lucas Oliveira de Paula - 241011386
+// Pedro Henrique Américo de Sá - 241025980
+
 int main() {
 
-    //printf("\n");
-    //printf("QUESTAO 2\n");
+    printf("\n");
+    printf("QUESTAO 2\n");
 
-    //printf("Digite o numero de chaves: ");
+    printf("Digite o numero de chaves: ");
     int numero_chaves;
     scanf("%d", &numero_chaves);
+    if (numero_chaves > 10 || numero_chaves <1){
+        printf("Numero invalido de chaves.\n");
+        return 1;
+    }
     
-    //printf("\n");
+    printf("\n");
 
-    //printf("Digite as chaves: ");
+    printf("Digite as chaves: ");
     int vetor[numero_chaves];
     for(int i = 0; i < (numero_chaves); i++){
         scanf("%d", &vetor[i]);
+        if (vetor[i] < 2|| vetor[i] > 20){
+        printf("Numero de ciclo invalido.\n");
+        return 1;
+    }
     }
 
     int ativas = 0;
     int ano = 0;
 
-    //printf("\n");
-    //printf("Começando Verificacao\n");
+    printf("\n");
+    printf("Começando Verificacao\n");
 
     while (ativas == 0 && ano < 50){
 
         ano++;
         ativas = 1;
         
-        //printf("\n");
-        //printf("Ano: %d\n", ano);
+        printf("\n");
+        printf("Ano: %d\n", ano);
 
         for(int i = 0; i < (numero_chaves); i++){
             if (ano % vetor[i] != 0){
                 ativas = 0;
-                //printf("Ano %d: Chave %d esta inativo.\n", ano, vetor[i]);
+                printf("Ano %d: Chave %d esta inativo.\n", ano, vetor[i]);
                 break;
             }
         }
